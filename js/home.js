052e6e084a7bc40cb69cbb341b4de8fbe5dd4cb5
Mammad <thisismammad7@gmail.com>
2024-07-04 18:10:43 +0330

    fixed read more on buttom of posts

--- js/home.js
+++ js/home.js
@@ -1,26 +1,29 @@
- $(".owl-carousel").owlCarousel({
+$(".owl-carousel").owlCarousel({
   loop: false,
   rtl: false,
   margin: 0,
   dots: false,
   nav: false,
   center: false,
-  responsive:{
-    0:{
-        items:3
+  responsive: {
+    0: {
+      items: 3,
     },
-    425:{
-        items:4
-    }
-}
+    380: {
+      items: 4,
+    },
+  },
 });
 
+// const readMore = document.querySelectorAll(".read-more");
 
-const readMore = document.querySelector("main .posts-container .post-item .post-footer .discription >span");
+// readMore.addEventListener("click", ()=>{
+//   const p = document.querySelector("main .posts-container .post-item .post-footer .discription > p");
+//   p.style.height = "auto";
+//   readMore.style.display = "none"
+// })
 
-
-readMore.addEventListener("click", ()=>{
-  const p = document.querySelector("main .posts-container .post-item .post-footer .discription > p");
-  p.style.height = "auto";
-  readMore.style.display = "none"
-})
+$(".read-more").on("click", function () {
+  $(this).parent().find("p").css("height", "auto");
+  $(this).css("display", "none");
+});
