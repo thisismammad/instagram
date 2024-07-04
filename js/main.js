f7945527a3d00e3decbf691d1578bbcfcf3409c0
Mammad <thisismammad7@gmail.com>
2024-07-05 00:04:33 +0330

    add dark them based on device color scheme

---
+++ js/main.js
@@ -0,0 +1,24 @@
+const root = document.querySelector(":root");
+
+
+
+
+function setDark(){
+    root.style.setProperty("--bg-color", "rgb(34,34,34)")
+    root.style.setProperty("--font-color", "rgb(230,230,230)")
+    root.style.setProperty("--input-color", "#363636")
+}
+function setLight(){
+    root.style.setProperty("--bg-color", "white")
+    root.style.setProperty("--font-color", "rgb(34, 34, 34)")
+    root.style.setProperty("--input-color", "#eee")
+}
+
+const isDark = window.matchMedia("(prefers-color-scheme:dark)").matches;
+console.log(isDark)
+
+if(isDark){
+    setDark();
+}else{
+    setLight()
+}
