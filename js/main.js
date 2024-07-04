b8c98e750ebef86edbd1eea0200e713b3f0b5417
Mammad <thisismammad7@gmail.com>
2024-07-05 00:06:45 +0330

    remove log

--- js/main.js
+++ js/main.js
@@ -1,24 +1,20 @@
 const root = document.querySelector(":root");
 
-
-
-
-function setDark(){
-    root.style.setProperty("--bg-color", "rgb(34,34,34)")
-    root.style.setProperty("--font-color", "rgb(230,230,230)")
-    root.style.setProperty("--input-color", "#363636")
+function setDark() {
+  root.style.setProperty("--bg-color", "rgb(34,34,34)");
+  root.style.setProperty("--font-color", "rgb(230,230,230)");
+  root.style.setProperty("--input-color", "#363636");
 }
-function setLight(){
-    root.style.setProperty("--bg-color", "white")
-    root.style.setProperty("--font-color", "rgb(34, 34, 34)")
-    root.style.setProperty("--input-color", "#eee")
+function setLight() {
+  root.style.setProperty("--bg-color", "white");
+  root.style.setProperty("--font-color", "rgb(34, 34, 34)");
+  root.style.setProperty("--input-color", "#eee");
 }
 
 const isDark = window.matchMedia("(prefers-color-scheme:dark)").matches;
-console.log(isDark)
 
-if(isDark){
-    setDark();
-}else{
-    setLight()
+if (isDark) {
+  setDark();
+} else {
+  setLight();
 }
