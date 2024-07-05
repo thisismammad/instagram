3a1e67731c336b4f9ba7c3fdcd6188bf91d17ce8
Mammad <thisismammad7@gmail.com>
2024-07-05 16:56:45 +0330

    fixed dark mode

--- js/main.js
+++ js/main.js
@@ -11,24 +11,10 @@ function setDark() {
     root.style.setProperty("--font-color", "rgb(34, 34, 34)");
     root.style.setProperty("--input-color", "#eee");
   }
-  let isDark = window.matchMedia("(prefers-color-scheme:dark)").matches;
-  
-  $(".dark-light").on("click",function(){
-    if (isDark) {
-        setLight();
-        $(".fa-moon").fadeIn(200);
-        $(".fa-sun-bright").fadeOut(200);
-        isDark = false;
-      } else {
-        setDark();
-        $(".fa-sun-bright").fadeIn(200);
-        $(".fa-moon").fadeOut(200);
-        isDark = true;
-      }
-  })
-  
-  
-  if (isDark) {
+
+ isDark=document.cookie.split(" ")[0]
+ 
+  if (isDark === "true") {
     setDark();
     $(".fa-sun-bright").fadeIn(200);
     $(".fa-moon").fadeOut(200);
--- js/profile.js
+++ js/profile.js
@@ -1,12 +1,48 @@
-
 $(".fa-arrow-left-long").on("click", function () {
-    $("aside").css("right", "-120%");
-    $("aside .head").css("right", "-120%");
-    $("body").css("overflow","auto")
+  $("aside").css("right", "-120%");
+  $("aside .head").css("right", "-120%");
+  $("body").css("overflow", "auto");
 });
 $(".fa-bars").on("click", function () {
-    $("aside").css("right", "0");
-    $("aside .head").css("right", "0");
-    $("body").css("overflow","hidden")
-
+  $("aside").css("right", "0");
+  $("aside .head").css("right", "0");
+  $("body").css("overflow", "hidden");
 });
+
+let isDark = document.cookie.split(" ")[0];
+
+if (isDark === "true") {
+  setDark();
+  $(".fa-sun-bright").fadeIn(200);
+  $(".fa-moon").fadeOut(200);
+} else {
+  setLight();
+  $(".fa-moon").fadeIn(200);
+  $(".fa-sun-bright").fadeOut(200);
+}
+
+$(".dark-light").on("click", function () {
+  if (isDark === "true") {
+    console.log("if");
+    setLight();
+    $(".fa-moon").fadeIn(200);
+    $(".fa-sun-bright").fadeOut(200);
+    isDark = "false";
+  } else {
+    setDark();
+    console.log("el");
+    $(".fa-sun-bright").fadeIn(200);
+    $(".fa-moon").fadeOut(200);
+    isDark = "true";
+  }
+  deleteAllCookies();
+  document.cookie = isDark + " expires=Wed, 1 Jan 2070 13:47:11 UTC; path=/";
+});
+
+function deleteAllCookies() {
+  document.cookie.split(";").forEach((cookie) => {
+    const eqPos = cookie.indexOf("=");
+    const name = eqPos > -1 ? cookie.substring(0, eqPos) : cookie;
+    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+  });
+}
